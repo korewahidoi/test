63471941753e11b37d1bdb345c6877a9ca2914df
korewahidoi <sidewinder.android@gmail.com>
2014-07-13 17:27:38 +0900

    test
    abcde

---
+++ src/test/Korewahidoi.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package test;
+
+/**
+ * @author Yoshinobu
+ *
+ */
+public class Korewahidoi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("ぬるぽ\ntest");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
